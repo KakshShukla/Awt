85f483af5a8577f03d6bc6842a13b2567ce18ecd
KakshShukla <108360067+KakshShukla@users.noreply.github.com>
2022-07-13 22:20:32 +0530

    Add files via upload

---
+++ 200103101041_Javascript/demo (1).js
@@ -0,0 +1,21 @@
+function ExtAlert()
+            {
+                alert("This world is goona suck!!!");
+            }
+function ExtConfirm()
+            {
+                if(confirm("Am i right...?"))
+                {
+                    alert("ofcourse");
+                }
+                else
+                {
+                    alert("Maybe not");
+                } 
+            }
+function ExtPrompt()
+            {
+                var fname=prompt("Tell your shit:");
+                var lname=prompt("Tell your shit more:");
+                alert(fname+""+lname);
+            }
